dc40c25f16f8356db368eff45468663804997952
Winston Tsang <rubyconsumer@rubyconsumer.com>
2011-11-07 18:35:02 -0500

    Ability to restart Solr (stop and start) with one rake task.

--- sunspot_rails/lib/sunspot/rails/tasks.rb
+++ sunspot_rails/lib/sunspot/rails/tasks.rb
@@ -23,6 +23,15 @@ namespace :sunspot do
       Sunspot::Rails::Server.new.stop
     end
 
+    desc 'stop the Solr instance and restart Solr afterwards'
+    task :restart => :environment do
+      if RUBY_PLATFORM =~ /w(in)?32$/
+        abort('This command does not work on Windows. Please use rake sunspot:solr:run to run Solr in the foreground.')
+      end
+      Sunspot::Rails::Server.new.stop
+      Sunspot::Rails::Server.new.start
+    end
+
     task :reindex => :"sunspot:reindex"
   end
 
